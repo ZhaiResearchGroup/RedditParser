e64a3578b94b1c4256df17fe73299be4ca23cda4
Aaron Green <aaron.green281@gmail.com>
2017-10-11 09:49:55 -0500

    recursive comment collector

--- reddit_parser.py
+++ reddit_parser.py
@@ -19,20 +19,51 @@ def get_posts(subreddits, limit, user_agent=default_user_agent):
 def get_post_comments(post_url, user_agent=default_user_agent):
     """Returns the comment data for a specified post."""
     response_data = requests.get(post_url, headers = {'User-agent': user_agent})
-    comment_data = response_data.json()[1]
+    post_data = response_data.json()[1]
 
-    some_comments = comment_data['data']['children']
-    for comment in some_comments:
-        if 'body' in comment['data']:
-            print(comment['data']['body'])
+    return get_post_comments_recur(post_data, [])
+
+def get_post_comments_recur(comment, comments):
+    """Recursive helper function to gather all comments in a Reddit thread.
+    
+    Right now this function is only getting 200 comments because that is all is shown on the
+    JSON page for the post. There are properties called 'more' with lists of id's. Perhaps 
+    those can be a link to a complete set of comments.
+    """
+    if 'data' in comment:
+        comment_data = comment['data']
+
+        # a new comment exists at this layer, add it to the total list of comments
+        if 'body' in comment_data:
+            new_comment = {
+                "score": comment_data['score'],
+                "body": comment_data['body'],
+                "subreddit": comment_data['subreddit'],
+                "author": comment_data['author']
+            }
+            comments.append(new_comment)
+
+        # recurse on children
+        if 'children' in comment_data:
+            for child in comment_data['children']:
+                comments = get_post_comments_recur(child, comments)
+
+        # recurse on replies
+        if 'replies' in comment_data:
+            comments = get_post_comments_recur(comment_data['replies'], comments)
+
+    return comments
 
 if __name__ == "__main__":
-    subreddits = ['all', 'news', 'worldnews', 'programmerhumor']
+    subreddits = ['news', 'worldnews', 'programmerhumor']
     limit = 500
     user_agent = 'ResearchBot'
 
     posts = get_posts(subreddits, limit)
     
     post_url = 'https://www.reddit.com' + posts[0]['data']['permalink'] + '.json'
+
     print(post_url)
-    get_post_comments(post_url)
+
+    post_comments = get_post_comments(post_url)
+    print(json.dumps(post_comments))
