cde40739e2f5c5a265b21a6ae9b8f0c955ad36d0
Aaron Green <aaron.green281@gmail.com>
2017-10-12 15:17:46 -0500

    comment parents, posts, and example json

--- reddit_parser.py
+++ reddit_parser.py
@@ -1,6 +1,9 @@
 import requests
 import json
+import uuid
 from reddit_constants import default_user_agent
+
+# example JSON is available in post.json, subreddit.json, and output.json
 
 def get_posts(subreddits, limit, user_agent=default_user_agent):
     """Returns a specified number of posts from a specified group of subreddits."""
@@ -21,9 +24,12 @@ def get_post_comments(post_url, user_agent=default_user_agent):
     response_data = requests.get(post_url, headers = {'User-agent': user_agent})
     post_data = response_data.json()[1]
 
-    return get_post_comments_recur(post_data, [])
+    # right now this gets the title, eventually convert to unique id for each title
+    post_id = response_data.json()[0]['data']['children'][0]['data']['title']
 
-def get_post_comments_recur(comment, comments):
+    return get_post_comments_recur(post_data, [], -1, post_id)
+
+def get_post_comments_recur(comment, comments, parent_comment_id, parent_post_id):
     """Recursive helper function to gather all comments in a Reddit thread.
     
     Right now this function is only getting 200 comments because that is all is shown on the
@@ -33,24 +39,31 @@ def get_post_comments_recur(comment, comments):
     if 'data' in comment:
         comment_data = comment['data']
 
+        new_comment = None
+
         # a new comment exists at this layer, add it to the total list of comments
         if 'body' in comment_data:
             new_comment = {
                 "score": comment_data['score'],
                 "body": comment_data['body'],
                 "subreddit": comment_data['subreddit'],
-                "author": comment_data['author']
+                "author": comment_data['author'],
+                "parent_comment_id": parent_comment_id,
+                "parent_post_id": parent_post_id,
+                "id": str(uuid.uuid4())
             }
             comments.append(new_comment)
+
+        next_parent_comment_id = parent_comment_id if new_comment is None else new_comment['id']
 
         # recurse on children
         if 'children' in comment_data:
             for child in comment_data['children']:
-                comments = get_post_comments_recur(child, comments)
+                comments = get_post_comments_recur(child, comments, next_parent_comment_id, parent_post_id)
 
         # recurse on replies
         if 'replies' in comment_data:
-            comments = get_post_comments_recur(comment_data['replies'], comments)
+            comments = get_post_comments_recur(comment_data['replies'], comments, next_parent_comment_id, parent_post_id)
 
     return comments
 
@@ -60,7 +73,7 @@ if __name__ == "__main__":
     user_agent = 'ResearchBot'
 
     posts = get_posts(subreddits, limit)
-    
+
     post_url = 'https://www.reddit.com' + posts[0]['data']['permalink'] + '.json'
 
     print(post_url)
