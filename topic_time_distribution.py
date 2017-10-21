9cb96ee5f0ddb2d7d4f021bf0b9dcd1d02d63d24
Aaron Green <Aaron Green>
2017-10-21 17:15:08 -0500

    topic time dist

--- topic_time_distribution.py
+++ topic_time_distribution.py
@@ -3,8 +3,9 @@ from sklearn.decomposition import LatentDirichletAllocation as LDA
 import numpy as np
 import reddit_parser
 
+from datetime import datetime, timedelta
 import sys
-
+from matplotlib import pyplot as plt
 
 def get_bodies(items):
     """Gets all of the bodies associated with inputted posts or comments"""
@@ -32,9 +33,48 @@ def build_documents(post_pairs):
 
     return documents
 
+def get_min_max_time(posts):
+    """Returns the oldest and newest times for any post in the set"""
+    min = sys.maxsize
+    max = -sys.maxsize - 1
+
+    for post in posts:
+        if post['created'] < min:
+            min = post['created']
+        if post['created'] > max:
+            max = post['created']
+
+    return (min, max)
+
+def generate_time_range(min_time, max_time, num_topics):
+    """Create an empty map of the time ranges from the min time to the max time"""
+    times = {}
+
+    while min_time < max_time:
+        times[min_time] = np.zeros(num_topics)
+        min_time += timedelta(hours = 1)
+
+    return times
+
+def increment_time_range_for_topic(time_topic_distribution, topic, timestamp):
+    """Increments the topic count at a specific time in the distribution"""
+    for time in time_topic_distribution:
+        if timestamp < time:
+            time_topic_distribution[time][topic] += 1
+            break
+
+def format_topic_counts(time_topic_distribution):
+    topic_counts = [[], [], [], [], []]
+    for time in time_topic_distribution:
+        topics = time_topic_distribution[time]
+        for i in range(0, len(topics)):
+            topic_counts[i].append(topics[i])
+
+    return topic_counts
+
 if __name__ == "__main__":
     subreddits = ['news']
-    limit = 10
+    limit = 100
     user_agent = "ResearchBot"
     num_topics = 5
     num_words = 10
@@ -42,9 +82,9 @@ if __name__ == "__main__":
     posts = reddit_parser.format_posts(reddit_parser.get_posts(subreddits, limit))
     post_bodies = get_bodies(posts)
     timestamps = [post["created"] for post in posts]
+    datetime_stamps = [str(datetime.fromtimestamp(timestamp)) for timestamp in timestamps]
 
     documents = build_documents(zip(posts, post_bodies))
-    document_time_pairs = zip(documents, timestamps)
 
     # michael's code
     vectorizer = CountVectorizer()
@@ -59,13 +99,14 @@ if __name__ == "__main__":
             index_to_word[index] = word
         except UnicodeEncodeError:
             print("")
-    # end michael's code
 
     lda = LDA(n_topics=num_topics)
     lda.fit(document_word_vectors)
 
-    # michael's code
+    # p(word|topic)  (n_topics X n_words)
     w_z = lda.components_ / lda.components_.sum(axis=1)[:, np.newaxis]
+
+    # p(topic|document)  (n_documents X n_topics)
     z_d = lda.transform(document_word_vectors)
 
     top_word_args = np.argsort(w_z, axis=1)[:,-1*num_words:]
@@ -75,4 +116,35 @@ if __name__ == "__main__":
         top_words[i,:,1] = w_z[i,top_word_args[i]]
     # end michael's code
 
-    print(top_words)
+    min_time, max_time = get_min_max_time(posts)
+
+    datetime_min_time = datetime.fromtimestamp(min_time)
+    datetime_max_time = datetime.fromtimestamp(max_time)
+
+    time_topic_distribution = generate_time_range(datetime_min_time, datetime_max_time, num_topics)
+
+    document_topics = np.argmax(z_d, axis = 1)
+
+    for i in range(0, len(z_d)):
+        doc_timestamp = timestamps[i]
+        doc_datetime = datetime.fromtimestamp(doc_timestamp)
+        document_topic = document_topics[i]
+
+        increment_time_range_for_topic(time_topic_distribution, document_topic, doc_datetime)
+
+    print(time_topic_distribution)
+
+    # plotting is messed up.
+
+    # topic_counts = format_topic_counts(time_topic_distribution)
+
+    # num_bars = len(timestamps)
+    # width = .35
+    # for topic_count in topic_counts:
+    #     plt.bar(num_bars, topic_count, width)
+
+    # plt.xlabel('Times')
+    # plt.ylabel('Topic Counts')
+    # plt.xticks(np.arange(num_bars), datetime_stamps)
+    # plt.yticks(np.arange(0, 10, 1))
+    # plt.show()
